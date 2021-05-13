9cb820bd8baf716af421a4275a132781ca49c8d4
William Santos <william.wapsantos@gmail.com>
2021-05-13 19:20:16 -0300

    Verificacao incial de consistencia

--- plan_consistency/views.py
+++ plan_consistency/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from .open_dcm import OpenRTPlan
 
 # Create your views here.
 
 def rtplan_consistency(request):
     context = {}
     if request.method == 'POST':
-        upload_dcm = request.FILES['document']
-        fs = FileSystemStorage()
-        file_name = fs.save(upload_dcm.name, upload_dcm)
-        context['file_url'] = fs.url(file_name)
+        upload_dcm = request.FILES['rtplan_dcm']
+        rtplan = OpenRTPlan()
+        rtplan.OpenRTPlanDCM(upload_dcm)
+        context['output_text'] = rtplan.PrintPlanParameter()
+        #fs = FileSystemStorage()
+        #file_name = fs.save(upload_dcm.name, upload_dcm)
+        #file_url = fs.url(file_name)
+        #context['file_url'] = fs.url(file_name)
     return render(request, 'plan_consistency/consistency.html', context)
+
