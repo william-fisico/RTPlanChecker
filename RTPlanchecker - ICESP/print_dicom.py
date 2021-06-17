d26520cf06413ed999c919c207251ff1fb5a2605
William Santos <64668896+william-fisico@users.noreply.github.com>
2021-06-17 19:12:44 -0300

    Delete RTPlanchecker - ICESP directory

--- RTPlanchecker - ICESP/print_dicom.py
+++
@@ -1,9 +0,0 @@
-from DCM_manager import Manager
-
-#x = Manager('./files/RTPlan.dcm')
-#x = Manager('./files/RTPlan_TESTEIMRT.dcm')
-x = Manager('./files/RTPlan_2Rx.dcm')
-#x = Manager('./files/RTPlan_isocentro.dcm')
-#x = Manager('./files/WL_2021-05-11a.pdf')
-
-x.print_dicom()
